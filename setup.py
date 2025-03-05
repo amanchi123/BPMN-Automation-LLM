14236e5f3553ec81c5c37a32a6c411f8708260f4
Akhila <akhilamanchi96@gmail.com>
2025-03-05 22:01:22 +0530

    notebook added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bpmn_automation",
+    version="0.0.0",
+    author = "Akhila",
+    author_email = "akhilamanchi96@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+
+
